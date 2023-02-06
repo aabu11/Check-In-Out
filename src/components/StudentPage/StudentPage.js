667227e9874065eb79824d3168631e861ee34de8
Ahmed Abu <ahmedabu13@gmail.com>
2023-02-06 16:12:08 -0600

    installed MUI and started Card formatting

--- src/components/StudentPage/StudentPage.js
+++ src/components/StudentPage/StudentPage.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 function StudentPage() {
   const dispatch = useDispatch();
   const student = useSelector(store => store.student)
@@ -40,7 +43,24 @@ function StudentPage() {
 }
 return (
     <div> Student List 
-
+          {student.map((student) =>{
+             return (
+              <Card variant="outlined" sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <div> 
+                    Student Name: {student.name}
+                  </div>
+                  <div> 
+                    Student Age: {student.age}
+                  </div>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Check In Student</Button>
+                </CardActions>
+              </Card>
+            );
+          })}
+       
     </div>
 )
 }
